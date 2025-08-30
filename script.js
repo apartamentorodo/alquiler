ace4ad77b2a53d0f1af75141065e99405df6d6cb
apartamentorodo <alquilerapartamentorodo@gmail.com>
2025-08-30 16:14:55 -0300

    Update script.js15

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ var captionText = document.getElementById("caption");
   modal.style.display = "block";
   modalImg.src = this.src;
   }
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+spanOnclick = function() {
   modal.style.display = "none";
 }
